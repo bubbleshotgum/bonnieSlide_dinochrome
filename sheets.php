f4dce369e16a455fbddf573d92e4c9db0bf5a1da
Mikich Esaian <woknsushi@yandex.ru>
2022-06-15 21:12:22 +0300

    preparing to deploy

--- sheets.php
+++ sheets.php
@@ -9,24 +9,27 @@
     $client_id = getenv("ClientID");
     $client_secret = getenv("ClientSecret");
     $refresh_uri = "https://accounts.google.com/o/oauth2/token";
-    try {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $refresh_uri);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-            "client_id" => $client_id,
-            "client_secret" => $client_secret,
-            "refresh_token" => $refresh_token,
-            "grant_type" => "refresh_token"
-        ]));
-        $res = curl_exec($ch);
 
-        echo json_encode(["message" => $refresh_token . " " . $client_id . " " . $client_secret]);
+    echo $refresh_token;
 
-        curl_close($ch);
-    } catch(Exception $e) {
-        echo json_encode(["message" => "Something went wrong"]);
-    }
+    // try {
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_URL, $refresh_uri);
+    //     curl_setopt($ch, CURLOPT_POST, true);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+    //         "client_id" => $client_id,
+    //         "client_secret" => $client_secret,
+    //         "refresh_token" => $refresh_token,
+    //         "grant_type" => "refresh_token"
+    //     ]));
+    //     $res = curl_exec($ch);
+
+    //     echo json_encode(["message" => $refresh_token . " " . $client_id . " " . $client_secret]);
+
+    //     curl_close($ch);
+    // } catch(Exception $e) {
+    //     echo json_encode(["message" => "Something went wrong"]);
+    // }
 
     // $request_uri = "https://sheets.googleapis.com/v4/spreadsheets/1OPT9rExu4-ILrHDFHF0HZoCzVVa-_4e4rsKrmfRiXR8/values/'Записи клиентов'";
     
