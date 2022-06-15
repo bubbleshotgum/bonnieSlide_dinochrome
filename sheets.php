228e1dbc5c8a95ac19f7ce5537cadedf4c861f78
Mikich Esaian <woknsushi@yandex.ru>
2022-06-15 21:05:28 +0300

    preparing to deploy

--- sheets.php
+++ sheets.php
@@ -1,5 +1,5 @@
 <?php
-    require_once realpath(__DIR__ . "vendor/autoload.php");
+    require_once realpath(__DIR__ . "/vendor/autoload.php");
 
     use Dotenv\Dotenv;
     $dotenv = Dotenv::createImmutable(__DIR__);
