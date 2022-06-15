564ad67a43e56e12f86e63ec1dcaed6ced8e80a5
Mikich Esaian <woknsushi@yandex.ru>
2022-06-16 01:16:10 +0300

    preparing to deploy

--- sheets.php
+++ sheets.php
@@ -16,8 +16,8 @@
     try {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $refresh_uri);
-        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
             "client_id" => $client_id,
             "client_secret" => $client_secret,
@@ -26,9 +26,26 @@
         ]));
         $res = curl_exec($ch);
         $decoded = json_decode($res);
-        echo json_encode($decoded);
+        $access_token = $decoded['access_token'];
         curl_close($ch);
     } catch(Exception $e) {
         echo json_encode(["message" => "Something went wrong"]);
+    }
+
+    $request_uri = "https://sheets.googleapis.com/v4/spreadsheets/1OPT9rExu4-ILrHDFHF0HZoCzVVa-_4e4rsKrmfRiXR8/values/";
+    
+    try {
+        echo json_encode(["message" => $access_token]);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "Authorization: Bearer " . $access_token
+        ]);
+        curl_setopt($ch, CURLOPT_URL, $request_uri . "A:A?majorDimension=COLUMNS");
+        $res = curl_exec($ch);
+        $decoded = json_decode($res);
+        echo json_encode($decoded);
+    } catch (OAuthException $e) {
+        echo json_encode(["message" => $e]);   
     }
 ?>
