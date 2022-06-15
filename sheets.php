ab1b81a6ff67f327dd0734051ca1151419bf867c
Mikich Esaian <woknsushi@yandex.ru>
2022-06-15 21:14:55 +0300

    preparing to deploy

--- sheets.php
+++ sheets.php
@@ -5,31 +5,26 @@
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
-    $refresh_token = getenv("RefreshToken");
-    $client_id = getenv("ClientID");
-    $client_secret = getenv("ClientSecret");
+    $refresh_token = $_ENV["RefreshToken"];
+    $client_id = $_ENV["ClientID"];
+    $client_secret = $_ENV["ClientSecret"];
     $refresh_uri = "https://accounts.google.com/o/oauth2/token";
 
-    echo $refresh_token;
-
-    // try {
-    //     $ch = curl_init();
-    //     curl_setopt($ch, CURLOPT_URL, $refresh_uri);
-    //     curl_setopt($ch, CURLOPT_POST, true);
-    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-    //         "client_id" => $client_id,
-    //         "client_secret" => $client_secret,
-    //         "refresh_token" => $refresh_token,
-    //         "grant_type" => "refresh_token"
-    //     ]));
-    //     $res = curl_exec($ch);
-
-    //     echo json_encode(["message" => $refresh_token . " " . $client_id . " " . $client_secret]);
-
-    //     curl_close($ch);
-    // } catch(Exception $e) {
-    //     echo json_encode(["message" => "Something went wrong"]);
-    // }
+    try {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $refresh_uri);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+            "client_id" => $client_id,
+            "client_secret" => $client_secret,
+            "refresh_token" => $refresh_token,
+            "grant_type" => "refresh_token"
+        ]));
+        $res = curl_exec($ch);
+        curl_close($ch);
+    } catch(Exception $e) {
+        echo json_encode(["message" => "Something went wrong"]);
+    }
 
     // $request_uri = "https://sheets.googleapis.com/v4/spreadsheets/1OPT9rExu4-ILrHDFHF0HZoCzVVa-_4e4rsKrmfRiXR8/values/'Записи клиентов'";
     
