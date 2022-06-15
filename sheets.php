95e7fe58eea759e0e6f4bf225f5b784c5a8078c1
Mikich Esaian <woknsushi@yandex.ru>
2022-06-16 00:37:53 +0300

    preparing to deploy

--- sheets.php
+++ sheets.php
@@ -25,7 +25,7 @@
         ]));
         $res = curl_exec($ch);
         $decoded = json_decode($res);
-        $access_token = $decoded['access_token'];
+        $access_token = $decoded['access_token'] ?: 15;
         curl_close($ch);
     } catch(Exception $e) {
         echo json_encode(["message" => "Something went wrong"]);
