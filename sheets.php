3ca80f205877ce2711b27f11ce04d432510180fb
Mikich Esaian <woknsushi@yandex.ru>
2022-06-15 21:01:34 +0300

    preparing to deploy

--- sheets.php
+++ sheets.php
@@ -1,4 +1,10 @@
 <?php
+    require_once realpath(__DIR__ . "vendor/autoload.php");
+
+    use Dotenv\Dotenv;
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
     $refresh_token = getenv("RefreshToken");
     $client_id = getenv("ClientID");
     $client_secret = getenv("ClientSecret");
