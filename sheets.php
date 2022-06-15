3e2eb4fac0baa0fdd02c827ec307f9e0d2da3c11
Mikich Esaian <woknsushi@yandex.ru>
2022-06-15 20:27:27 +0300

    preparing to deploy

--- sheets.php
+++ sheets.php
@@ -16,9 +16,9 @@
             "refresh_token" => $refresh_token
         ]));
         $res = curl_exec($ch);
-        // echo json_encode(["message" => curl_error($ch)]);
+        echo json_encode(["message" => curl_error($ch)]);
         curl_close($ch);
-        echo json_encode($res[0]);
+        // echo json_encode($res[0]);
         // return json_decode($res);
     } catch(Exception $e) {
         echo json_encode(["message" => "Fuck you"]);
